b36850a1ebc14c157bad0306b466bf19a866873b
Mário Malta da Silva a22007457 <79096482+mariosilva22007457@users.noreply.github.com>
2023-01-07 19:56:39 +0000

    novos

--- src/typeorm/entities/Patient.ts
+++ src/typeorm/entities/Patient.ts
@@ -1,6 +1,35 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Double, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({name:'patients'})
 export class Patient{
+    @PrimaryGeneratedColumn({ type: 'bigint' })
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    createdAt: Date;
+
+    @Column()
+    age: number;
+
+
+    @Column()
+    dob: Date;
+
+    @Column()
+    weight : Double;
+
+    @Column()
+    height: Double;
+
+    @Column()
+    diabetes : boolean;
     
+    @Column({ nullable: true })
+    authStrategy: string;
 }
